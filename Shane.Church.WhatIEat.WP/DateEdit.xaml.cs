bdff8e48b97491704ae01c6638a98f60f99df92f
Shane Church <shane@s-church.net>
2013-04-30 16:57:20 -0600

    Initial App Store Submission

--- Shane.Church.WhatIEat.WP/DateEdit.xaml.cs
+++ Shane.Church.WhatIEat.WP/DateEdit.xaml.cs
@@ -22,7 +22,39 @@ namespace Shane.Church.WhatIEat.WP
 		public DateEdit()
 		{
 			InitializeComponent();
+
+			InitializeAdControl();
 		}
+
+		#region Ad Control
+		private void InitializeAdControl()
+		{
+			if (Microsoft.Devices.Environment.DeviceType == Microsoft.Devices.DeviceType.Emulator)
+			{
+				AdControl.ApplicationId = "test_client";
+				AdControl.AdUnitId = "Image480_80";
+			}
+			else
+			{
+				AdControl.ApplicationId = "111c32e1-4679-4ad0-9af0-c971de0562fa";
+				AdControl.AdUnitId = "128211";
+			}
+#if PERSONAL
+			AdControl.IsEnabled = false;
+			AdControl.Height = 0;
+#endif
+		}
+
+		private void AdControl_ErrorOccurred(object sender, Microsoft.Advertising.AdErrorEventArgs e)
+		{
+			AdControl.Height = 0;
+		}
+
+		private void AdControl_AdRefreshed(object sender, EventArgs e)
+		{
+			AdControl.Height = 80;
+		}
+		#endregion
 
 		private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
 		{
--- Shane.Church.WhatIEat.WP/MainCalendar.xaml.cs
+++ Shane.Church.WhatIEat.WP/MainCalendar.xaml.cs
@@ -32,10 +32,42 @@ namespace Shane.Church.WhatIEat.WP
 
 			InitializeApplicationBar();
 
+			InitializeAdControl();
+
 			_model = KernelService.Kernel.Get<MainViewModel>();
 			var appointmentSource = new EntriesAppointmentSource(_model);
 			this.MainRadCalendar.AppointmentSource = appointmentSource;
 		}
+
+		#region Ad Control
+		private void InitializeAdControl()
+		{
+			if (Microsoft.Devices.Environment.DeviceType == Microsoft.Devices.DeviceType.Emulator)
+			{
+				AdControl.ApplicationId = "test_client";
+				AdControl.AdUnitId = "Image480_80";
+			}
+			else
+			{
+				AdControl.ApplicationId = "111c32e1-4679-4ad0-9af0-c971de0562fa";
+				AdControl.AdUnitId = "128211";
+			}
+#if PERSONAL
+			AdControl.IsEnabled = false;
+			AdControl.Height = 0;
+#endif
+		}
+
+		private void AdControl_ErrorOccurred(object sender, Microsoft.Advertising.AdErrorEventArgs e)
+		{
+			AdControl.Height = 0;
+		}
+
+		private void AdControl_AdRefreshed(object sender, EventArgs e)
+		{
+			AdControl.Height = 80;
+		}
+		#endregion
 
 		protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
 		{
