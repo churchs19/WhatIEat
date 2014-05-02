12555097e00cd8c77134e4f4b6d18d01f153df76
Shane Church <shane@s-church.net>
2014-05-02 12:31:39 -0600

    Meal Type picker positioning corrected

---
+++ Shane.Church.WhatIEat.WP/Helpers/PreviousSiblingConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Shane.Church.WhatIEat.WP.Helpers
+{
+    public class PreviousSiblingConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is FrameworkElement)
+            {
+                var elem = value as FrameworkElement;
+                if (elem.Parent != null)
+                {
+                    var childCount = VisualTreeHelper.GetChildrenCount(elem.Parent);
+                    var itemIdx = -1;
+                    for (var i = 0; i < childCount; i++)
+                    {
+                        var item = VisualTreeHelper.GetChild(elem.Parent, i);
+                        if (item.Equals(elem))
+                        {
+                            itemIdx = i;
+                        }
+                    }
+                    if (itemIdx > 0)
+                    {
+                        return VisualTreeHelper.GetChild(elem.Parent, itemIdx - 1);
+                    }
+                }
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
